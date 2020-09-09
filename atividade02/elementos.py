ca96ad626a832491c29656ed25d45cf893252f87
JulianoQueiroz <68660839+JulianoQueiroz@users.noreply.github.com>
2020-09-09 18:54:29 -0300

    lista duplamente encadeada

--- atividade02/elementos.py
+++ atividade02/elementos.py
@@ -4,12 +4,13 @@ para inserir elementos, uma para imprimir os elementos na ordem que foram inseri
 função para imprimir os elementos na ordem inversa a que foram inseridos. '''
 
 class Elemento:
-    def __init__(self, valor1, valor2):
+    def __init__(self, valor1):
         self.elementosOrdenados = valor1
-        self.elementosInversos = valor2
         self.proximo = None
 
-   
+    def __init__(self, valor2):
+        self.elementosInversos = valor2
+        self.proximo = None
 
 
     
