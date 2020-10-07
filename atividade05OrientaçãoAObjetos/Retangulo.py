5a252c782111a92437d2633f9e4354bc85dfaa24
JulianoQueiroz <68660839+JulianoQueiroz@users.noreply.github.com>
2020-10-07 17:20:15 -0300

    Calculo do retângulo
    POO

---
+++ atividade05OrientaçãoAObjetos/Retangulo.py
@@ -0,0 +1,13 @@
+# Construa um algoritmo para implementar a classe Retângulo que possui os atributos: altura e largura.
+# A classe deve ter os seguintes métodos:
+# ○ Método construtor
+# ○ Método que calcula a área do retângulo ( altura * largura) e retorna o resultado
+# ○ Método que imprime os valores dos atributos da classe.
+
+class Retangulo:
+    def __init__(self, altura, largura):
+        self.altura = altura
+        self.largura = largura
+
+    def calcularArea(self):
+        return self.altura * self.largura
--- atividade05OrientaçãoAObjetos/main.py
+++ atividade05OrientaçãoAObjetos/main.py
@@ -1,42 +1,9 @@
-# Construa um algoritmo para implementar a classe Retângulo que possui os atributos: altura e largura.
-# A classe deve ter os seguintes métodos:
-# ○ Método construtor
-# ○ Método que calcula a área do retângulo ( altura * largura) e retorna o resultado
-# ○ Método que imprime os valores dos atributos da classe.
+from Retangulo import Retangulo
 
-class Retangulo:
-    def __init__(self):
-        self.altura = None
-        self.largura = None
-    
-    def altura(self, altura):
-        altura = self.altura(altura) 
+largura = int(input('Informe a largura: ')) 
+altura = int(input('Informe o comprimento: ')) 
 
-    def altura(self, largura):
-        largura = self.largura(largura)
+retangulo = Retangulo(largura, altura)
+area = retangulo.calcularArea()
 
-    def calculaArea(self, altura, largura):
-        calculaArea = altura * largura
-        return calculaArea
-
-while True:
-    e = input('''
-    0  - Finlizar
-    01 - Adicionar altura
-    02 - Adicionar largura
-    03 - Calaculo do retângulo
-    Escolha: \n
-    ''')
-    if e == '0':
-        break
-    if e == '1':
-        altura = float(input("Digite um valor para altura: "))
-        Retangulo.altura(altura)
-    if e == '2':
-        largura = float(input("Digite um valor para largura: "))
-        Retangulo.largura(altura)
-    if e == '3':
-        Retangulo.calculaArea()
-
-resultado = Retangulo()
-
+print(" O caluculo da área do retangulo é: " + str(area))
